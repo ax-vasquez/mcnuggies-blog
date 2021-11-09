091eb948e83394f4d98d91d930cfc01b343fb784
Armando Vasquez <ar.xa.vasquez@gmail.com>
2021-11-08 23:39:29 -0600

    employment history component updates (#45)

--- web/src/components/about/EmploymentHistoryJobTitle.tsx
+++ web/src/components/about/EmploymentHistoryJobTitle.tsx
@@ -3,12 +3,22 @@ import styled from 'styled-components'
 import { SanityJobTitle } from '../../../graphql-types'
 import { THEME } from '../../style/colors'
 
+const StyledJobTitleDiv = styled.div`
+    margin-left: 4rem;
+`
+
 const StyledJobTitleSpan = styled.span`
     display: inline-flex;
     width: 100%;
     align-items: center;
     & {
+        h4 {
+            margin-top: 0;
+            margin-bottom: 0;
+        }
         p {
+            margin-top: 0;
+            margin-bottom: 0;
             padding-left: 1rem;
             font-style: italic;
             color: ${THEME.light.font.accent};
@@ -16,9 +26,25 @@ const StyledJobTitleSpan = styled.span`
     }
 `
 
+const StlyedResponsibilitiesList = styled.ul`
+    border-left: solid 3px;
+    border-color: ${THEME.light.border.default};
+    padding: 0px !important;
+    margin-left: 1rem;
+`
+
+/**
+ * The — symbol is the "em dash", which is the "long dash" symbol. To enter it (on linux),
+ * press CTRL+SHIFT+U > type `2014` and press `ENTER`
+ */
+const StyledResponsibilitiesListItem = styled.li`
+    margin: 1rem 0 1rem 0;
+    list-style-type: none;
+`
+
 const EmploymentHistoryJobTitle = ({ jobTitle }: { jobTitle: SanityJobTitle }) => {
     return (
-        <div>
+        <StyledJobTitleDiv>
             <StyledJobTitleSpan>
                 <h4>{jobTitle.title}</h4>
                 <p>
@@ -29,10 +55,14 @@ const EmploymentHistoryJobTitle = ({ jobTitle }: { jobTitle: SanityJobTitle }) =
                     {(jobTitle.currentJobTitle) ? 'Present' : jobTitle.endDate}
                 </p>
             </StyledJobTitleSpan>
-            <ul>
-                {jobTitle.responsibilities.map((responsibility) => <li>{responsibility}</li>)}
-            </ul>
-        </div>
+            <StlyedResponsibilitiesList>
+                {jobTitle.responsibilities.map((responsibility) => (
+                    <StyledResponsibilitiesListItem>
+                        {responsibility}
+                    </StyledResponsibilitiesListItem>
+                  ))}
+            </StlyedResponsibilitiesList>
+        </StyledJobTitleDiv>
     )
 }
 
--- web/src/components/about/EmploymentHistoryList.tsx
+++ web/src/components/about/EmploymentHistoryList.tsx
@@ -1,8 +1,20 @@
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import React from 'react'
 import styled from 'styled-components'
 import { SanityEmployer } from '../../../graphql-types'
 import { THEME } from '../../style/colors'
 import EmploymentHistoryJobTitle from './EmploymentHistoryJobTitle'
+
+const StyledEmployerRow = styled.div`
+    display: inline-flex;
+    vertical-align: middle;
+    margin: 1rem 0 1rem 0;
+    & {
+        h3 {
+            margin: 0 0 0 0.5rem; 
+        }
+    }
+`
 
 const StyledEmploymentHistoryList = styled.ul`
     list-style-type: none !important;
@@ -15,6 +27,22 @@ const StyledEmploymentHistoryListItem = styled.li`
     border-width: 2px;
     border-color: ${THEME.light.border.default};
     padding-bottom: 2rem;
+    padding-left: 0 !important;
+    & {
+        ul {
+            padding-left: 0;
+        }
+    }
+`
+
+const StyledJobTitleListItem = styled.li`
+    list-style-type: none;
+    padding-left: 0 !important;
+`
+
+const StyledEmployerIcon = styled(GatsbyImage)`
+    width: 2rem;
+    height: 2rem;
 `
 
 const EmploymentHistoryList = ({ employers }: { employers: SanityEmployer[] }) => {
@@ -24,17 +52,21 @@ const EmploymentHistoryList = ({ employers }: { employers: SanityEmployer[] }) =
             <StyledEmploymentHistoryList>
                 {employers.length > 0
                     ? employers.map((employer) => {
+                        const employerIcon = getImage(employer.image.asset.gatsbyImageData)
                         return (
                             <StyledEmploymentHistoryListItem>
-                                <div>
+                                <StyledEmployerRow>
+                                    <a href={employer.homePage} target="_blank" rel="noreferrer">
+                                        <StyledEmployerIcon image={employerIcon} alt="employer_image" />
+                                    </a>
                                     <h3>{employer.name}</h3>
-                                </div>
+                                </StyledEmployerRow>
                                 <ul>
                                     {employer.jobTitles.map((jobTitle) => {
                                         return (
-                                            <li>
+                                            <StyledJobTitleListItem>
                                                 <EmploymentHistoryJobTitle jobTitle={jobTitle} />
-                                            </li>
+                                            </StyledJobTitleListItem>
                                         )
                                     })}
                                 </ul>
