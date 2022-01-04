9173923811e0e7e292bf2fecab9140da60db2f45
Armando Vasquez <ar.xa.vasquez@gmail.com>
2022-01-03 19:24:23 -0600

    dank favicon

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -42,6 +42,20 @@ module.exports = {
                 allExtensions: true, // defaults to false
             },
         },
-        'gatsby-plugin-styled-components'
+        'gatsby-plugin-styled-components',
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#663399`,
+                // This will impact how browsers show your PWA/website
+                // https://css-tricks.com/meta-theme-color-and-trickery/
+                // theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/mcnuggies.png`, // This path is relative to the root of the site.
+            },
+        },
     ],
 }
