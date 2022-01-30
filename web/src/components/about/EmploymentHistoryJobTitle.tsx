e73c9b37369135595c8d55981f8e3c64d66563b0
Armando Vasquez <ar.xa.vasquez@gmail.com>
2022-01-30 14:11:22 -0600

    make job title component use sass (#73)

--- web/src/components/about/EmploymentHistoryJobTitle.tsx
+++ web/src/components/about/EmploymentHistoryJobTitle.tsx
@@ -1,53 +1,7 @@
 import React from 'react'
-import styled from 'styled-components'
 import { SanityJobTitle } from '../../../graphql-types'
 import { MONTHS } from '../../pages/blog'
-import { THEME } from '../../style/colors'
-import { device } from '../../style/devices'
-
-const StyledJobTitleDiv = styled.div`
-    @media ${device.mobileS} {
-        margin-left: 1rem;
-    }
-    @media ${device.tablet} {
-        margin-left: 4rem;
-    }
-`
-
-const StyledJobTitleSpan = styled.span`
-    display: inline-flex;
-    width: 100%;
-    align-items: center;
-    & {
-        h4 {
-            margin-top: 0;
-            margin-bottom: 0;
-        }
-        p {
-            margin-top: 0;
-            margin-bottom: 0;
-            padding-left: 1rem;
-            font-style: italic;
-            color: ${THEME.light.font.accent};
-        }
-    }
-`
-
-const StlyedResponsibilitiesList = styled.ul`
-    border-left: solid 3px;
-    border-color: ${THEME.light.border.default};
-    padding: 0px !important;
-    margin-left: 1rem;
-`
-
-/**
- * The — symbol is the "em dash", which is the "long dash" symbol. To enter it (on linux),
- * press CTRL+SHIFT+U > type `2014` and press `ENTER`
- */
-const StyledResponsibilitiesListItem = styled.li`
-    margin: 1rem 0 1rem 0;
-    list-style-type: none;
-`
+import * as styles from './EmploymentHistoryList.module.scss'
 
 const EmploymentHistoryJobTitle = ({ jobTitle }: { jobTitle: SanityJobTitle }) => {
     const {
@@ -69,8 +23,8 @@ const EmploymentHistoryJobTitle = ({ jobTitle }: { jobTitle: SanityJobTitle }) =
     }
 
     return (
-        <StyledJobTitleDiv>
-            <StyledJobTitleSpan>
+        <div className={styles.jobTitleContainer}>
+            <span>
                 <h4>{title}</h4>
                 <p>
                     {startDateString}
@@ -79,15 +33,15 @@ const EmploymentHistoryJobTitle = ({ jobTitle }: { jobTitle: SanityJobTitle }) =
                     {` `}
                     {(currentJobTitle) ? `Present` : endDateString}
                 </p>
-            </StyledJobTitleSpan>
-            <StlyedResponsibilitiesList>
+            </span>
+            <ul className={styles.responsibilitiesList}>
                 {responsibilities.map((responsibility) => (
-                    <StyledResponsibilitiesListItem key={`responsibility-${responsibility}`}>
+                    <li key={`responsibility-${responsibility}`}>
                         {responsibility}
-                    </StyledResponsibilitiesListItem>
+                    </li>
                   ))}
-            </StlyedResponsibilitiesList>
-        </StyledJobTitleDiv>
+            </ul>
+        </div>
     )
 }
 
