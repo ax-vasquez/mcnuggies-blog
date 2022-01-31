5bf9d741fb8fe34299755d6f476fe21ff8884fa7
Armando Vasquez <ar.xa.vasquez@gmail.com>
2022-01-30 18:23:25 -0600

    remove styled-components - complete switch to sass (#76)

--- web/src/components/projects/ProjectRow.tsx
+++ web/src/components/projects/ProjectRow.tsx
@@ -1,40 +1,7 @@
 import React from 'react'
-import styled from 'styled-components'
 import BlockContent from '@sanity/block-content-to-react'
-import { THEME } from '../../style/colors'
-
-const StyledProjectsListItem = styled.li`
-    margin-bottom: 1rem;
-    padding: 1rem;
-    border-top: solid;
-    border-bottom: solid;
-    border-width: 2px;
-    border-color: ${THEME.light.border.default};
-`
-
-const StyledIntegrationImage = styled.img`
-    height: 1.5rem;
-`
-
-const StyledProjectHeaderRow = styled.div`
-    display: inline-flex;
-    align-items: center;
-    & {
-        h2 {
-            padding: 0;
-            margin: 0;
-        }
-        a {
-            margin-right: 1rem;
-            background: none;
-            padding: 0.25rem;
-            border-radius: 2px;
-            &:hover {
-                background: #DFDFDF;
-            }
-        }
-    }
-`
+import * as styles from './ProjectRow.module.scss'
+import ghLogo from '../../images/octocat-logo.png'
 
 const ProjectRow = ({
     title,
@@ -47,17 +14,17 @@ const ProjectRow = ({
 }) => {
 
     return (
-        <StyledProjectsListItem>
+        <li className={styles.container}>
             <div>
-                <StyledProjectHeaderRow>
+                <div className={styles.headerRow}>
                     <a href={repoUrl} target="_blank" rel="noreferrer">
-                        <StyledIntegrationImage src="/GitHub-Mark-64px.png" alt="gh-logo" />
+                        <img className={styles.integrationImage} src={ghLogo} alt="gh-logo" />
                     </a>
                     <h2>{title}</h2>
-                </StyledProjectHeaderRow>
+                </div>
                 <BlockContent blocks={_rawDescription} />
             </div>
-        </StyledProjectsListItem>
+        </li>
     )
 }
 
