b02673330b8ed4f01da4ef1e2a20731cd74c7bb3
Armando Vasquez <ar.xa.vasquez@gmail.com>
2022-01-30 18:23:25 -0600

    remove styled-components - complete switch to sass (#76)

--- web/src/pages/404.tsx
+++ web/src/pages/404.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
-import styled from 'styled-components'
 import { Helmet } from 'react-helmet'
 import Layout from '../components/Layout'
-import { THEME } from '../style/colors'
+import * as styles from './404.module.scss'
 
 export const query = graphql`
 query{
@@ -15,31 +14,6 @@ query{
        )
     }
 }
-`
-
-const StyledNotFoundImageContainerDiv = styled.div`
-    justify-content: center;
-    display: flex;
-    position: relative;
-`
-
-const StyledNotFoundImage = styled(GatsbyImage).attrs({
-    className: ``,
-})`
-    height: auto;
-    width: 100vw;
-`
-
-const StyledNotFoundImageOverlayDiv = styled.div`
-    bottom: 0px;
-    background-color: rgba(0, 0, 0, 0.25);
-    color: ${THEME.light.font.nav};
-    justify-content: center;
-    display: flex;
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    align-items: center;
 `
 
 const NotFoundPage = ({ data }: { data: {
@@ -53,16 +27,16 @@ const NotFoundPage = ({ data }: { data: {
                 <meta charSet="utf-8" />
                 <title>404</title>
             </Helmet>
-            <StyledNotFoundImageContainerDiv>
-                <StyledNotFoundImage image={image} alt="404-image" />
-                <StyledNotFoundImageOverlayDiv>
+            <div className={styles.imageContainer}>
+                <GatsbyImage image={image} alt="404-image" />
+                <div className={styles.overlay}>
                     <h2>
                         <code>404</code>
                         {` `}
                         - page not found
                     </h2>
-                </StyledNotFoundImageOverlayDiv>
-            </StyledNotFoundImageContainerDiv>
+                </div>
+            </div>
         </Layout>
     )
 }
