6c12aa97d96d240399c8f17089daf35ebd5a6667
Armando Vasquez <ar.xa.vasquez@gmail.com>
2022-03-06 18:43:07 -0600

    Page generation (#83)
    * generate pages from sanity.io data (#83)
    
    * pr fixes: remove groq - it wasn't doing anything

---
+++ next/components/feedItem/FeedItem.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+import React, {FunctionComponent} from 'react'
+import styles from './FeedItem.module.scss'
+
+interface FeedItemProps {
+    title: string
+    href: string
+    image?: any
+    subtitle?: string
+    textContent: React.ReactNode
+}
+
+const FeedItem: FunctionComponent<FeedItemProps> = ({
+    title,
+    href,
+    image,
+    subtitle,
+    textContent
+}) => {
+    return (
+        <div className={styles.container}>
+            <Link
+                href={href}
+            >
+                <div>
+                    <h2>{title}</h2>
+                    {textContent}
+                </div>
+            </Link>
+        </div>
+        
+        
+    )
+}
+
+export default FeedItem
