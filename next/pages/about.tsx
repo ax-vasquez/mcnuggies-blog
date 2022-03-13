90f5fe01fd4885f6a1e50c80e9bd30223f54c7b1
Armando Vasquez <ar.xa.vasquez@gmail.com>
2022-03-12 22:33:14 -0600

    lint studio too (#87)

--- next/pages/about.tsx
+++ next/pages/about.tsx
@@ -1,15 +1,35 @@
 import { NextPage } from 'next'
 import React from 'react'
 import { PageLayout } from '../components/layout/PageLayout'
+import client from '../sanity/client'
 
 const About: NextPage = () => {
     return (
 	<PageLayout
             pageTitle='About'
+            imgSrc='/about.jpg'
         >
 
 	</PageLayout>
     )
+}
+
+export async function getStaticProps() {
+    const allArticles = await client.fetch(`
+      *[_type == "creator"]{
+          name,
+          "imageUrl": image.asset->url,
+          githubUrl,
+          linkedInUrl,
+          bio
+      }
+    `)
+
+    return {
+      props: {
+        allArticles
+      }
+    }
 }
 
 export default About
--- studio/schemas/documents/jobTitle.ts
+++ studio/schemas/documents/jobTitle.ts
@@ -1,51 +1,51 @@
 export default {
-    name: 'jobTitle',
-    title: 'Job Title',
-    type: 'document',
-    fields: [
-      {
-        name: 'title',
-        title: 'Title',
-        type: 'string',
-        validation: (Rule) => Rule.required(),
-      },
-      {
-        title: 'Start date',
-        name: 'startDate',
-        type: 'date',
-        options: {
-          dateFormat: 'YYYY-MM',
-          calendarTodayLabel: 'Today',
-        },
-        validation: (Rule) => Rule.required(),
-      },
-      {
-        title: 'End date',
-        name: 'endDate',
-        type: 'date',
-        options: {
-          dateFormat: 'YYYY-MM',
-          calendarTodayLabel: 'Today',
-        },
-      },
-      {
-        title: 'Current job title?',
-        name: 'currentJobTitle',
-        type: 'boolean',
-      },
-      {
-        title: 'Responsibilities',
-        name: 'responsibilities',
-        type: 'array',
-        of: [{ type: 'string' }],
-      },
-    ],
-    preview: {
-        select: {
-          title: 'title',
-        },
+  name: `jobTitle`,
+  title: `Job Title`,
+  type: `document`,
+  fields: [
+    {
+      name: `title`,
+      title: `Title`,
+      type: `string`,
+      validation: (Rule) => Rule.required(),
     },
-    initialValue: {
-        currentJobTitle: false,
+    {
+      title: `Start date`,
+      name: `startDate`,
+      type: `date`,
+      options: {
+        dateFormat: `YYYY-MM`,
+        calendarTodayLabel: `Today`,
+      },
+      validation: (Rule) => Rule.required(),
     },
+    {
+      title: `End date`,
+      name: `endDate`,
+      type: `date`,
+      options: {
+        dateFormat: `YYYY-MM`,
+        calendarTodayLabel: `Today`,
+      },
+    },
+    {
+      title: `Current job title?`,
+      name: `currentJobTitle`,
+      type: `boolean`,
+    },
+    {
+      title: `Responsibilities`,
+      name: `responsibilities`,
+      type: `array`,
+      of: [{type: `string`}],
+    },
+  ],
+  preview: {
+    select: {
+      title: `title`,
+    },
+  },
+  initialValue: {
+    currentJobTitle: false,
+  },
 }
