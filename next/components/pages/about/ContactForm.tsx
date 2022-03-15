a3a8596bfffc1da5396709e15a5b6c6b18ae5687
Armando Vasquez <ar.xa.vasquez@gmail.com>
2022-03-14 22:17:28 -0500

    WIP - create base form - need to link to herotofu

---
+++ next/components/pages/about/ContactForm.tsx
@@ -0,0 +1,69 @@
+import React, { FunctionComponent, useState } from 'react'
+import { Button } from 'react-bootstrap'
+import styles from './ContactForm.module.scss'
+
+// TODO: 
+const FORM_ENDPONT = ``
+
+export const ContactForm: FunctionComponent = () => {
+    const [submitted, setSubmitted] = useState(false)
+
+    const handleSubmit = () => {
+        setTimeout(() => {
+            setSubmitted(true)
+        }, 100)
+    }
+
+    if (submitted) {
+        return (
+          <div className={styles.submitted}>
+            <span>Thank you!</span>
+            <p>We&apos;ll be in touch soon</p>
+          </div>
+        )
+    }
+
+    return (
+      <form
+        action={FORM_ENDPONT}
+        onSubmit={handleSubmit}
+        method='POST'
+        target='_blank'
+        className={styles.contactForm}
+      >
+        <div className={styles.inlineInput}>
+          <span>Name:</span>
+          <input
+                type='text'
+                name='name'
+                className=''
+                required
+            />
+        </div>
+        <div className={styles.inlineInput}>
+          <span>email:</span>
+          <input
+                type='text'
+                name='email'
+                className=''
+                required
+            />
+        </div>
+        <div className={styles.textArea}>
+          <textarea
+                placeholder='Message'
+                name='message'
+                className=''
+                required
+            />
+        </div>
+        <div className={styles.controls}>
+          <Button
+            type='submit'
+          >
+            Submit
+          </Button>
+        </div>
+      </form>
+    )
+}
