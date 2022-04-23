186a6e0a94f9985aa897647d2290bfb9b9fe4ee8
Armando Vasquez <ar.xa.vasquez@gmail.com>
2022-04-23 15:24:14 -0500

    Filter blog feed by search (#95)
    * category filtering cleanup
    
    * base control styles
    
    * WIP - need to fix category filtering combined with searching
    
    * WIP
    
    * fix spacing and filter logic
    
    * fix category filtering
    
    * category filtering fix and cleanup
    
    * doc comments
    
    * add NoMatchFound component

--- web/next/pages/blog-feed.tsx
+++ web/next/pages/blog-feed.tsx
@@ -7,6 +7,7 @@ import {PortableText} from '@portabletext/react'
 import FeedItem from "../components/feedItem/FeedItem"
 import { SanityBlock, SanityKeyed } from 'sanity-codegen'
 import { Category } from '../components/shared/Category'
+import { NoMatchFound } from '../components/pages/blog-feed/NoMatchFound'
 
 type ArticleResponse = {
   title: string
@@ -25,7 +26,15 @@ interface NextPageProps {
 const BlogFeed: NextPage<NextPageProps> = ({ allArticles }) => {
 
     const [activeCategories, setActiveCategories] = useState([] as string[])
+    const [searchText, setSearchText] = useState(``)
 
+    /**
+     * Handler to add/remove active categories to the stateful array
+     * 
+     * If the `activeCategories` list already contains the given category string,
+     * then it's removed from the array. Otherwise, the string is added to the 
+     * `activeCategories` stateful array.
+     */
     const categoryFilterHandler = (category: string) => {
       if (activeCategories.includes(category)) {
         const copy = [...activeCategories]
@@ -37,63 +46,103 @@ const BlogFeed: NextPage<NextPageProps> = ({ allArticles }) => {
       }
     }
 
+    /**
+     * Helper method to grab all categories belonging to the given article
+     */
+    const categoriesForArticle = (article: ArticleResponse) => {
+      const articleCategoriesObj = {}
+      article.categories.forEach(cat => articleCategoriesObj[cat.title] = 0)
+      return Object.keys(articleCategoriesObj)
+    }
+
+    /**
+     * Helper method to determine if an article should be shown, based on the currently-selected active
+     * categories.
+     */
+    const articleShouldBeVisible = (article: ArticleResponse) => {
+      const articleCategories = categoriesForArticle(article)
+      if (activeCategories.every(activeCategory => articleCategories.includes(activeCategory))) return true
+      return false
+    }
+
+    /**
+     * Memoized list of all *distinct* categories, extracted from the current list of articles.
+     */
     const allCategories = useMemo(() => {
       const obj = {}
       allArticles.forEach(article => article.categories.forEach(cat => obj[cat.title] = 0))
       return Object.keys(obj).sort()
     }, [allArticles])
 
+    /**
+     * Memoized list of articles to show
+     * 
+     * If no active categories are selected **and** there is no search text, all articles are displayed ("unfiltered view").
+     * 
+     * If active categories are selected:
+     * * The only articles shown are those who:
+     *    * Have every active category listed in their categories and...
+     *    * ARE NOT missing any active categories (in other words, if 4 categories are active, but a given article only has 3 of them, then that article won't be shown)
+     * * Searching by text should further-filter the **subset** of articles (meaning, it should only search the articles whose categories are
+     *    currently active)
+     */
     const shownArticles = useMemo(() => {
-      if (activeCategories.length === 0) {
+      if (activeCategories.length === 0 && searchText.length === 0) {
         return allArticles
       }
 
-      const shownArticles = [] as ArticleResponse[]
-      allArticles.forEach((article) => {
-        if (activeCategories.every(activeCat => {
-          const obj = {}
-          article.categories.forEach(cat => obj[cat.title] = 0)
-          const categoriesForArticle = Object.keys(obj)
-          console.log(`IS ${activeCat} IN: `, categoriesForArticle)
-          if (categoriesForArticle.includes(activeCat)) {
-            console.log(`YES`)
-            return true
+      const shownArticles = [...allArticles] as ArticleResponse[]
+      if (activeCategories.length > 0) {
+        allArticles.forEach((article) => {
+          if (!articleShouldBeVisible(article)) {
+            shownArticles.splice(shownArticles.indexOf(shownArticles.filter(art => art.title === article.title)[0]), 1)
           }
-          console.log(`NO`)
-          return false
-        })) {
-          shownArticles.push(article)
-        }
-      })
+        })
+      }
+      if (searchText.length > 0) {
+        return shownArticles.filter(article => article.title.toLowerCase().includes(searchText.toLowerCase()))
+      }
+      
       return shownArticles
-    }, [activeCategories, allArticles])
+    }, [activeCategories, allArticles, searchText])
+
+
 
     return (
       <PageLayout
             pageTitle="Blog"
             imgSrc="/generic-blog.jpeg"
         >
-        <div className='blog-controls'>
-          <ul className='categories-filter'>{allCategories.map(category => (<Category isActive={activeCategories.includes(category)} onClick={() => categoryFilterHandler(category)} key={`cat-item-${kebabCase(category)}`} title={category} />))}</ul>
-        </div>
-        <div className="blog-feed">
-          {shownArticles
-          .map(article => {
-                    const rowKey = `article-row-${kebabCase(article.title).toLowerCase()}`
-                    return (
-                      <FeedItem
-                            title={article.title!}
-                            href={`/blog/${article.slug!.current.toLowerCase()}`}
-                            key={rowKey}
-                            textContent={(
-                              <PortableText
-                                    value={article.summary!}
-                                />
-                            )}
-                        />
-                    )
-                })}
-        </div>
+          <div className='blog-feed-container'>
+            <div className='blog-controls'>
+              <input placeholder='Search...' value={searchText} onChange={(e) => setSearchText(e.target.value)}/>
+              <ul className='categories-filter'>{allCategories.map(category => (<Category isActive={activeCategories.includes(category)} onClick={() => categoryFilterHandler(category)} key={`cat-item-${kebabCase(category)}`} title={category} />))}</ul>
+            </div>
+            <div className="blog-feed">
+              { shownArticles.length > 0 ?
+              
+              shownArticles
+              .map(article => {
+                        const rowKey = `article-row-${kebabCase(article.title).toLowerCase()}`
+                        return (
+                          <FeedItem
+                                title={article.title!}
+                                href={`/blog/${article.slug!.current.toLowerCase()}`}
+                                key={rowKey}
+                                textContent={(
+                                  <PortableText
+                                        value={article.summary!}
+                                    />
+                                )}
+                            />
+                        )
+                })  
+            :
+                <NoMatchFound query={searchText}/>
+            }
+            </div>
+          </div>
+        
       </PageLayout>
     )
 }
