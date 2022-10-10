9ccf1f34fd6f76f488df1a44a214936e8d384001
Armando Vasquez <ar.xa.vasquez@gmail.com>
2022-10-10 14:07:26 -0500

    Style fixes (#103)
    * home tile spacing fix
    
    * remove hero image from home page - serves no purpose
    
    * home page rework
    
    * improve sidebar functionality
    
    * fix to ensure menu button works with outer-click logic
    
    * fix icon alignment
    
    * remove private cypress version

--- web/next/components/layout/sidebar/Sidebar.tsx
+++ web/next/components/layout/sidebar/Sidebar.tsx
@@ -1,6 +1,7 @@
-import React, { FunctionComponent } from 'react'
-import { useSelector } from 'react-redux'
+import React, { FunctionComponent, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import kebabCase from '../../../util/kebabCase'
+import { toggleShowSidebar } from "../../../redux/sidebarSlice"
 import styles from './Sidebar.module.scss'
 import SidebarOption, { SidebarOptionConfig } from './SidebarOption'
 
@@ -30,11 +31,38 @@ const SIDEBAR_OPTIONS = [
 ] as SidebarOptionConfig[]
 
 const Sidebar: FunctionComponent<SidebarProps> = () => {
-
+    
+    const dispatch = useDispatch()
     const isSidebarOpen = useSelector((state: any) => state.nav.showSidebar)
 
+    /**
+     * Handler to close the sidebar when the user clicks outside of it
+     * 
+     */
+    function outerClickHandler(e) {
+        const sidebarElement = document.getElementById('sidebar')
+        const sidebarButton = document.getElementById('sidebar-menu-button')
+        if (sidebarElement && sidebarButton) {
+            if (!sidebarElement.contains(e.target as Node) && !sidebarButton.contains(e.target as Node)) {
+                dispatch(toggleShowSidebar())
+            }
+        }
+    }
+
+    useEffect(() => {
+        if (isSidebarOpen) {
+            window.addEventListener('click', outerClickHandler)
+        }
+        // Must remove the event listener on cleanup
+        return () => {
+            window.removeEventListener('click', outerClickHandler)
+        }
+    }, [isSidebarOpen])
+
+    
+
     return (
-      <div className={`${styles.sidebarContainer} ${isSidebarOpen ? undefined : styles.closed}`}>
+      <div id='sidebar' className={`${styles.sidebarContainer} ${isSidebarOpen ? undefined : styles.closed}`}>
         {SIDEBAR_OPTIONS.map(option => {
                 const key = kebabCase(option.option.label)
                 return (
--- web/next/pages/index.tsx
+++ web/next/pages/index.tsx
@@ -6,27 +6,61 @@ import { TechStackIntegration } from '../components/pages/home/TechStackIntegrat
 
 const Home: NextPage = () => {
   return (
-    <PageLayout
-      pageTitle='Home'
-      imgSrc='/building-blocks.jpg'
-    >
+    <PageLayout>
       <Card
         className='tech-stack'
       >
-        <br />
+        <h1>mcnuggies.dev</h1>
         <RootPageSection
           title='Blog'
           iconFileName='bootstrap-book'
           href='/blog-feed'
           description="My occasional guides, reviews and general musings"
         />
-        <br />
         <RootPageSection
           title='About'
           iconFileName='bootstrap-info-circle'
           href='/about'
           description="Find out more about mcnuggies.dev and its creator"
         />
+        <div className='welcome-message'>
+          <h2>Welcome!</h2>
+          <p>
+            My name is Armando Vasquez and I created this site as a personal blog and digital portfolio. I will update this site on a semi-regular basis, but as someone who is frequently-engaged
+            in a variety of random projects, I don't have much time to maintain this site. <b>My site is not dead, it's just sleepy. 🙂</b>
+          </p>
+          <h3>Coding</h3>
+          <p>
+            Part of the reason I created this blog is because I explore so many different languages, frameworks and tools that I can't possibly employ them all in my
+            personal projects. While I certainly will write about things related to my day job, I also intend to write exploratory pieces on things I may or may not ever utilize in a complete project.
+            From regular 'ole JavaScript web dev to automating modeling tasks in parametric modeling software such as FreeCAD; if it's code-related and I'm interested in it, odds are good it will
+            end up here in one of my blog posts.
+          </p>
+          <h3>Gaming</h3>
+          <p>
+            As an avid gamer, I have no shortage of thoughts on the gaming industry. However, I certainly don't consider myself a "pro", which is a little sad given the sheer amount of time I've spent
+            playing video games. As such, I will never write about "being good" at a game. That's just not me. I'm the guy repeatedly getting blown to pieces and posting nonsense in all-chat. But I'm
+            having fun while doing it.
+          </p>
+          <p>
+            What I <i>will</i> write about are more-technical pieces about game performance, setting up servers, or general observations about company and industry moves. While I lack in raw gaming skill,
+            I make up for it in technical know-how. I've dabbled in Unity, Unreal and Godot and have years of experience with JavaScript, Python, C++ and more. At a more end-user level, I've set up a variety of 
+            different game servers both self-hosted and through paid providers and have gained a wide range of experience in networking architecture through my day job as a full stack web developer.
+          </p>
+          <h3>3D Modeling</h3>
+          <p>
+            Some of my personal projects are more than pure software projects. As such, I've been a user of difficult-to-use, but free modeling software to aid in designing some of my projects. On the more
+            creative side of 3D modeling, I've used Blender for creating and animating organic character models. For projects that require mathematical precision (at least, more so than Blender can easily 
+            provide out-of-the-box), I use FreeCAD. Both Blender and FreeCAD are incredibly difficult to use coming from a place of no experience - as was the case for me. I intend to write a variety of
+            guides to help others get through issues I had to faceplant my way through. Hopefully, with minimal faceplanting of their own.
+          </p>
+          <h3>Miscellaneous Projects</h3>
+          <p>
+            As mentioned before, I take on a lot of projects. These projects are quite varied; from stock-market analysis tools to botanical automation to game development. It's pretty much impossible for me to
+            encapsulate all categories of my interests. With that said, all of these projects are very exciting to me and I will, no doubt, write about them at length.
+          </p>
+        </div>
+        
         <br />
         <br />
         <div className='title-container'>
