b726861aa2d4543ee16dd3c6e7bbc5ea61efb1e5
Armando Vasquez <ar.xa.vasquez@gmail.com>
2023-12-04 02:19:45 -0600

    Migrate sanity studio to v3 (#108)
    * migrate sanity studio to v3
    
    * remove old configuration file

---
+++ web/studio/sanity.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+import { codeInput } from '@sanity/code-input'
+import { dashboardTool } from "@sanity/dashboard"
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemas/schema'
+
+export default defineConfig({
+    name: 'default',
+    title: 'my-creators-blog',
+    projectId: '4t91dfsx',
+    dataset: 'production',
+    plugins: [
+      deskTool(),
+      codeInput(),
+      dashboardTool({}), // Dashboard is a Sanity Content Studio Tool which renders any widgets configured for it
+      visionTool()  // Vision is a plugin for Sanity Studio for testing GROQ queries
+    ],
+    schema: {
+        types: schemaTypes,
+    },
+})
