7da9a81608617b6ba8f2499b3143732ded216672
Armando Vasquez <ar.xa.vasquez@gmail.com>
2024-01-10 23:31:51 -0600

    general mobile UI fixes (#119)

--- web/next/components/layout/sidebar/SidebarOption.tsx
+++ web/next/components/layout/sidebar/SidebarOption.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 import React, { FunctionComponent } from 'react'
+import { useDispatch } from 'react-redux'
+import { toggleShowSidebar } from "../../../redux/sidebarSlice"
 
 interface OptionConfig {
     label: string
@@ -17,9 +19,10 @@ export interface SidebarOptionConfig {
 const SidebarOption: FunctionComponent<SidebarOptionConfig> = ({
     option
 }) => {
+  const dispatch = useDispatch()
     return (
       <div className='sidebar-option-row' id="sidebar-option">
-        <Link href={option.to!} passHref>
+        <Link href={option.to!} passHref onClick={() => dispatch(toggleShowSidebar())}>
           <div className='sidebar-option-row-content'>
             <span>
               {option.label}
