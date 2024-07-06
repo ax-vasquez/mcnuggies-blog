400e70ba6f94aa2dfe49404ea5f6ebec6eec6627
Armando Vasquez <mando@mcnuggies.dev>
2024-07-06 17:16:52 -0500

    query project data and log result (WIP)

--- web/next/pages/projects.tsx
+++ web/next/pages/projects.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import { PageLayout } from '../components/layout/PageLayout'
+import client from '../sanity/client'
+import { Project } from '../types/sanity'
 
-const Projects: React.FC = () => {
+interface ProjectsProps {
+  projects: Project[]
+}
+
+const Projects: React.FC<ProjectsProps> = ({
+  projects
+}) => {
+    console.log(`PROJECTS: `, projects)
     return (
       <PageLayout
             metaDescription={`Projects from the creator and maintainer of ${process.env.HOST}`}
@@ -10,6 +19,22 @@ const Projects: React.FC = () => {
         <h1>Projects</h1>
       </PageLayout>
     )
+}
+
+export async function getStaticProps() {
+
+  const projects = await client.fetch(`*[_type == "project"]{
+    title,
+    slug,
+    description,
+    repoUrl
+  } | order(title asc)`)
+
+  return {
+    props: {
+      projects
+    }
+  }
 }
 
 export default Projects
