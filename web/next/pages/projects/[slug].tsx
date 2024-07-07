d43c4974489760be89b208f9d88fe544adeb9365
Armando Vasquez <mando@mcnuggies.dev>
2024-07-07 16:59:57 -0500

    setup page route and base query logic for project details page

---
+++ web/next/pages/projects/[slug].tsx
@@ -0,0 +1,54 @@
+import { FunctionComponent } from "react"
+import { PageLayout } from "../../components/layout/PageLayout"
+import client from "../../sanity/client"
+import { Project } from "../../types/sanity"
+
+interface ProjectPageProps {
+    projectTitle: string
+}
+
+const ProjectPage: FunctionComponent<ProjectPageProps> = ({
+    projectTitle,
+}) => {
+
+    return (
+      <PageLayout
+            pageTitle={projectTitle}
+            useTitleOverlay={false}
+            metaDescription={`Project details for ${projectTitle}`}
+        >
+        <h1>{projectTitle}</h1>
+      </PageLayout>
+    )
+}
+
+export async function getStaticPaths() {
+    const paths = await client.fetch(
+      `*[_type == "project" && defined(slug.current)][].slug.current`
+    )
+
+    return {
+      paths: paths.map((slug: string) => ({params: {slug}})),
+      fallback: true,
+    }
+}
+
+export async function getStaticProps(context: any) {
+    const { slug = `` } = context.params
+
+    const project: Project = await client.fetch(`
+        *[_type == "project" && slug.current == $slug][0]{
+          title,
+          repoUrl,
+          description,
+        }
+    `, { slug: slug.toLowerCase() })
+
+    return {
+        props: {
+            projectTitle: project.title
+        }
+    }
+}
+
+export default ProjectPage
